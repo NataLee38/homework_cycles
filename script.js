6272501af68da7be8eaa94293f03928fbaac5cb2
NataLee38 <light3475house@gmail.com>
2021-07-26 22:34:48 +0300

    cycle task done

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-let result = 0;
+// Найти сумму чисел от 1 до 10
 
-for (let i = 1; i <= 10; i++) {
-    result += i;
-    console.log(result);
-}
+// let result = 0;
+// let i = 1;
+
+// for (let i = 1; i <= 10; i++) {
+//     result += i;
+//     console.log(result);
+// }
+
+// while (i <= 10) {
+//     result += i;
+//     console.log(result);
+//     i++;
+// }
+
+// do {
+//     result += i;
+//     console.log(result);
+//     i++;
+// } while (i <= 10);
+
